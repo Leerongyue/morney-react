3033e51b4ed85d72a62f78a73ab2c82e614d56ca
lirongyue <761780862@qq.com>
2020-05-20 10:06:48 +0800

    配置react hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
+import {
+  Redirect,
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 // import styled from 'styled-components';
 
 function App() {
   return (
-    <div>
-      fuck
-    </div>
+    <Router>
+      <div>
+        <Switch >
+          <Redirect exact from="/" to="/money"/>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">Tags</Link>
+            </li>
+            <li>
+              <Link to="/money">Money</Link>
+            </li>
+            <li>
+              <Link to="/statistics">Statistics</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
+
+}
+
+function NoMatch() {
+  return <h2>
+    页面不存在
+  </h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
 }
 
 export default App;
