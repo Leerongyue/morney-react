68f6ff0e19fc10a8eb418a1ec67c7283ff9c51df
lirongyue <761780862@qq.com>
2020-05-21 15:14:35 +0800

    给Icon添加链接

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,36 +5,44 @@ import Icon from './Icon';
 
 const NavWrapper = styled.div`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
-    >ul{
+  >ul{
      display: flex;
      justify-content: space-around;
        >li{
           padding:4px 0;
           line-height: 24px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-           >svg{
-           width:24px;
-           height:24px;
-           }
+             >a{
+                display: flex;
+                flex-direction: column;
+                 align-items: center;
+                   >svg{
+                       width:24px;
+                       height:24px;
+                   }
+             }
        }
-    }`;
+  }`;
 const Nav = () => {
   return (
     <NavWrapper>
       <ul>
         <li>
-          <Icon name={'tag'}/>
-          <Link to="/tags">Tags</Link>
+          <Link to="/tags">
+            <Icon name={'tag'}/>
+            标签
+          </Link>
         </li>
         <li>
-          <Icon name={'money'}/>
-          <Link to="/money">Money</Link>
+          <Link to="/money">
+            <Icon name={'money'}/>
+            记账
+          </Link>
         </li>
         <li>
-          <Icon name={'statistics'}/>
-          <Link to="/statistics">Statistics</Link>
+          <Link to="/statistics">
+            <Icon name={'statistics'}/>
+            统计
+          </Link>
         </li>
       </ul>
     </NavWrapper>
