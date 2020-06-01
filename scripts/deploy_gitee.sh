cb5aaf50a5fa892be844094867ca5f1dc0a21b36
lirongyue <761780862@qq.com>
2020-06-01 22:09:14 +0800

    update

--- scripts/deploy_gitee.sh
+++ scripts/deploy_gitee.sh
@@ -5,6 +5,6 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@gitee.com:leerongyue/morney-react-website.git &&
-git push -u origin master -f
+git remote add gitee git@gitee.com:leerongyue/morney-react-website.git &&
+git push -u gitee master -f
 cd -
--- scripts/deploy_github.sh
+++ scripts/deploy_github.sh
@@ -5,6 +5,6 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@github.com:Leerongyue/morney-react-website.git &&
-git push -u origin master -f
+git remote add github git@github.com:Leerongyue/morney-react-website.git &&
+git push -u github master -f
 cd -
